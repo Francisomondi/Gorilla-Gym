d01e648db05ab8cf8fd653ac2dc3c99fee2a3ce5
francis omondi <francisomondi17@gmail.com>
2023-05-19 22:35:18 +0300

    navbar complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 
 import './App.css'
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SelectedPage } from '@/shared/Types';
 
 
 function App() {
-  const [selectedPage,setSelectedPage]= useState("home")
+  const [selectedPage, setSelectedPage]= useState<SelectedPage>(SelectedPage.Home)
+  const [isTopOfPage, setisTopOfPage]= useState<boolean>(true)
+
+  useEffect(()=>{
+    const handleScroll=()=>{
+      if(window.scrollY === 0){
+        setisTopOfPage(true)
+
+        setSelectedPage(SelectedPage.Home)
+      } 
+    }
+   
+  })
 
 
   return (
     <div className='app bg-gray-50'>
       <Navbar
-       selectedPage={selectedPage}
-       setSelectedPage={setSelectedPage}/>
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -9,15 +9,17 @@ import ActionButton from "@/shared/ActionButton";
 
 
 type Props = {
+    isTopOfPage:boolean
     selectedPage: SelectedPage
     setSelectedPage:(value: SelectedPage) => void
 }
 
-const Navbar = ({selectedPage,setSelectedPage}: Props) => {
+const Navbar = ({isTopOfPage, selectedPage,setSelectedPage}: Props) => {
 
     const flexBetween='flex items-center justify-between'
     const isAboveMediumScreens= useMediaQuery("(min-width:1020px)")
     const [isMenuToggled, setIsMenuToggled]=useState<boolean>(false)
+    const navbarBackground = isTopOfPage ? " " : "bg-primary-100 drop-shadow"
 
 
 return (
@@ -72,6 +74,43 @@ return (
             </div>
 
         </div>
+
+        {/* Mobile menu model */}
+        {!isAboveMediumScreens && isMenuToggled &&(
+            <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100  drop-shadow-xl">
+                {/* close Icon*/}
+                <div className="flex justify-end p-12">
+                    <button onClick={()=>setIsMenuToggled(!isMenuToggled)}>
+                        <XMarkIcon className="h-6 w-6 text-gray-400"/>
+                    </button>
+
+                </div>
+                {/** menu items*/}
+                <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+                        <Link 
+                        page="Home"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                        />
+                        <Link 
+                        page="Benefits"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                        />
+                        <Link
+                        page="Our classes"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                        />
+                        <Link 
+                        page="Contacts"
+                        selectedPage={selectedPage}
+                        setSelectedPage={setSelectedPage}
+                        />
+                    </div>
+
+            </div>
+        )}
     </nav>
 )
 }
